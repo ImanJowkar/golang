849c977a890646818b9f6537d54976e9e71b7c01
Iman Jowkar <imanjowkar99@gmail.com>
2025-11-03 12:58:34 +0330

    update golang section

--- 1-variables/main.go
+++ 1-variables/main.go
@@ -28,4 +28,33 @@ func main() {
 
 	fmt.Println("the value of pi * 2 is : ", pi*2)
 
+
+	// data type can found automatically
+	e := 2.71
+	fmt.Println("the e is: ", e)
+
+	name1 := "saman"
+	fmt.Println("the name1 is: ", name1)
+
+	// const data types
+	const (
+		youtubeName = "Iman Jowkar"
+		cityBorn = "Shiraz"
+	)
+
+	var (
+		employeeName string = "Jack"
+		jobPosition string = "DevOps"
+		employeeID int32 = 1222254
+	)
+
+	yearsofService := 5
+	isFlltime := true
+	hourlyRate :=30.4
+
+
+	weekleyPay := hourlyRate * 3
+
+	fmt.Println(employeeName, jobPosition, employeeID, yearsofService, isFlltime, weekleyPay)
+
 }
